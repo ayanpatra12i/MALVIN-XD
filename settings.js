612093208de02a05513b7b189155d14184e835c9
ayanpatra12i <ayanpatra810@gmail.com>
2025-02-16 16:39:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~VoAA0TYZ#ZydzLbeuEOdIYNX7M9F3G2mH3SiBs0bbxCp6AyjYKvg",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~VoAA0TYZ#ZydzLbeuEOdIYNX7M9F3G2mH3SiBs0bbxCp6AyjYKvg","MALVIN-XD~JtQj0TIa#YucTJ42UEU1QERoQsqoKVpuFCFnJBEAr2ntDVBiT4MY",
 // add your Session Id 
 
 PREFIX: process.env.PREFIX || ".",
